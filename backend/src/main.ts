dbe556d4e9deff7f713af4f8197b1eeb3c4c98ec
Emmanuel Imanol Vaca Hernandez <emanuelimanol.vaca@gmail.com>
2026-02-28 15:19:56 -0600

    chore: update CORS with Vercel URL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3001',
+      'https://erp-system-one-pied.vercel.app/login',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
